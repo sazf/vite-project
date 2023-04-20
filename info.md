d9076086794bbaa830738123d57495d47abec820
Sazf <sazf@hotmail.com>
2023-04-20 15:46:36 -0500

    feat: agregar campos feat: mock del json

---
+++ info.md
@@ -0,0 +1,13 @@
+### Para crear un proyecto con Vite
+
+npm create vite@latest
+
+### Ejecutar el proyecto
+
+cd vite-project
+npm install
+npm run dev
+
+### un css básico para el proyecto
+
+https://watercss.kognise.dev/
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import response from './mocs/mantenimientoTipo.json';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const isData = response?.data.length > 0;
+  console.log('isData: ', isData);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='page'>
+      <header>
+        <h1>Mi buscador</h1>
+      </header>
+      <main>
+        <form>
+          <input type='text' placeholder='codigo' />
+          <button type='submit'>Buscar</button>
+        </form>
+        <div className='results'>
+          <ul>{isData && response.data.map((item) => <li key={item.mant_tipo_id}>{item.mant_tipo_descripcion}</li>)}</ul>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
